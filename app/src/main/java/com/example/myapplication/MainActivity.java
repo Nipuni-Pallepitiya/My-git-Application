1a64a1295c87943f714562288f29fc5930f0cbd5
Nipuni-Pallepitiya <npallepitiya@gmail.com>
2020-07-25 09:02:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       final Button myFirstButton=findViewById(R.id.btnclickMe);
+        myFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myFirstButton.setText("Hello");
+
+            }
+        });
+
+
+    }
+
+
+
+
+}
